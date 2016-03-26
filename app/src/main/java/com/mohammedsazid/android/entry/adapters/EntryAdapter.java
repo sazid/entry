ea3144637b2030f9d81b80597020520bcb410f2b
Mohammed Sazid-Al-Rashid <sazidozon@gmail.com>
2016-03-26 21:03:32 +0600

    Implement code for copy and delete of entries

--- app/src/main/java/com/mohammedsazid/android/entry/adapters/EntryAdapter.java
+++ app/src/main/java/com/mohammedsazid/android/entry/adapters/EntryAdapter.java
@@ -1,10 +1,16 @@
 package com.mohammedsazid.android.entry.adapters;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mohammedsazid.android.entry.Entry;
 import com.mohammedsazid.android.entry.R;
@@ -57,7 +63,8 @@ public class EntryAdapter extends RecyclerView.Adapter<EntryAdapter.EntryViewHol
         return new EntryViewHolder(v);
     }
 
-    public static class EntryViewHolder extends RecyclerView.ViewHolder {
+    public class EntryViewHolder extends RecyclerView.ViewHolder
+            implements View.OnLongClickListener {
         TextView entryTextTextView;
         TextView entryDateTextView;
         TextView entryTimeTextView;
@@ -68,6 +75,63 @@ public class EntryAdapter extends RecyclerView.Adapter<EntryAdapter.EntryViewHol
             entryTextTextView = (TextView) itemView.findViewById(R.id.entryTextTextView);
             entryDateTextView = (TextView) itemView.findViewById(R.id.entryDateTextView);
             entryTimeTextView = (TextView) itemView.findViewById(R.id.entryTimeTextView);
+
+            itemView.setLongClickable(true);
+            itemView.setOnLongClickListener(this);
+        }
+
+        @Override
+        public boolean onLongClick(final View v) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+            builder.setTitle("Actions")
+                    .setItems(new String[]{"Copy", "Delete"}, new EntryActionsDialog(v.getContext()));
+            builder.show();
+
+            return true;
+        }
+
+        private class EntryActionsDialog implements DialogInterface.OnClickListener {
+            Context context;
+
+            public EntryActionsDialog(Context context) {
+                this.context = context;
+            }
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                final Entry entry = EntryAdapter.this.mEntries.get(getAdapterPosition());
+
+                switch (which) {
+                    case 0:
+                        ClipboardManager clipboard = (ClipboardManager)
+                                context.getSystemService(Context.CLIPBOARD_SERVICE);
+                        ClipData clip = ClipData.newPlainText(entry.getEntryText(), entry.getEntryText());
+                        clipboard.setPrimaryClip(clip);
+
+                        Toast.makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        new AlertDialog.Builder(context)
+                                .setTitle("Confirm delete")
+                                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Thread t = new Thread() {
+                                            @Override
+                                            public void run() {
+                                                entry.delete(context.getContentResolver());
+                                            }
+                                        };
+                                        t.start();
+                                        mEntries.remove(getAdapterPosition());
+                                        notifyItemRemoved(getAdapterPosition());
+                                    }
+                                })
+                                .setNegativeButton("NO", null)
+                                .show();
+                        break;
+                }
+            }
         }
     }
 }
